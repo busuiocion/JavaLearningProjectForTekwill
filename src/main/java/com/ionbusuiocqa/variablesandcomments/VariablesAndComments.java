0933c8d1890511004ad5637452dd4cbb93913881
ionbusuioc <busuiocion.ib@gmail.com>
2024-10-16 15:13:36 -0700

    Home work - 1st lesson

---
+++ src/main/java/com/ionbusuiocqa/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,46 @@
+package com.ionbusuiocqa.variablesandcomments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+
+        // Declaration of variables
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimit0fByteDataType;
+        short lowerLimit0fShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimit0fIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimit0fLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimit0fDoubleDataType;
+        double upperLimit0fDoubleDataType;
+
+
+       // Initialization of the above variables
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        lowerLimitOfByteDataType = -128;
+        upperLimit0fByteDataType = 127;
+        lowerLimit0fShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimit0fIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimit0fLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimit0fDoubleDataType = -1.7E+308d;
+        upperLimit0fDoubleDataType = 1.7E+305;
+
+        System.out.println(lowerLimitOfByteDataType);
+    }
+}
